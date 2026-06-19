ad81501f82d942f3793c4a8802b28a01c4dc9a0a
dhruvL@21 <dhruvlukhi07@gmail.com>
2026-06-19 15:50:49 +0530

    solve login error

--- src/lib/amplify.ts
+++ src/lib/amplify.ts
@@ -4,7 +4,7 @@ Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: 'ap-south-1_aZbUeVRjX',
-      userPoolClientId: '1qj64pfbco37mgg1eln9pmamqb',
+      userPoolClientId: '1qj64pfbco37mqg1eln9pmamqb',
       signUpVerificationMethod: 'code',
     }
   }
