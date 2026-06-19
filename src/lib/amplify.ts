665cdd19c5e215a055e98afdb2fcd687281d6a11
dhruvL@21 <dhruvlukhi07@gmail.com>
2026-06-19 16:19:58 +0530

    changed deployment key

--- src/lib/amplify.ts
+++ src/lib/amplify.ts
@@ -1,10 +1,13 @@
 import { Amplify } from 'aws-amplify'
 
+const userPoolId = import.meta.env.VITE_COGNITO_USER_POOL_ID || 'ap-south-1_aZbUeVRjX';
+const userPoolClientId = import.meta.env.VITE_COGNITO_CLIENT_ID || '3mis9f4oecir3ikk56fuun3vsf';
+
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: 'ap-south-1_aZbUeVRjX',
-      userPoolClientId: '1qj64pfbco37mqg1eln9pmamqb',
+      userPoolId,
+      userPoolClientId,
       signUpVerificationMethod: 'code',
     }
   }
